fd1c2e6ee2bf2b7ff1de0a7a04ac74c00427fda8
Morten Bøgeskov <mb@dbc.dk>
2018-11-22 10:58:24 +0100

    Added basic utils

---
+++ utils/src/main/java/dk/dbc/opensearch/utils/Timing.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2018 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of opensearch-utils
+ *
+ * opensearch-utils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opensearch-utils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.opensearch.utils;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+public interface Timing extends AutoCloseable {
+
+//    private final Map<String, Long> timings;
+//    private final String name;
+//    private final long start;
+//
+//    Timing(Map<String, Long> timings, String name) {
+//        this.timings = timings;
+//        this.name = name;
+//        this.start = System.nanoTime();
+//    }
+
+    @Override
+    public void close();
+//        long ms = ( System.nanoTime() - start ) / 1_000_000L;
+//        timings.compute(name, (k, v) -> ms + v);
+//    }
+}
