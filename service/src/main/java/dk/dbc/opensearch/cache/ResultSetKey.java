85887bd5e93c52378da7f16956e30cbece13602c
Morten Bøgeskov <mb@dbc.dk>
2018-11-27 07:44:26 +0100

    Cache key from relevant query parts

---
+++ service/src/main/java/dk/dbc/opensearch/cache/ResultSetKey.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2018 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of opensearch-service
+ *
+ * opensearch-service is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opensearch-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.opensearch.cache;
+
+import dk.dbc.opensearch.input.CollectionType;
+import dk.dbc.opensearch.input.SearchRequest;
+import dk.dbc.opensearch.input.UserDefinedBoost;
+import dk.dbc.opensearch.input.UserDefinedRanking;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+public class ResultSetKey implements Serializable {
+
+    private static final long serialVersionUID = 7388581403133101704L;
+
+    private final Integer agencyId;
+    private final Boolean allObjects;
+    private final CollectionType collectionType;
+    private final List<String> profiles;
+    private final String query;
+    private final String repository;
+    private final Integer showAgencyId;
+    private final List<String> sort;
+    private final List<UserDefinedBoost> userDefinedBoost;
+    private final List<UserDefinedRanking> userDefinedRanking;
+
+    public static ResultSetKey of(SearchRequest req) {
+        return new ResultSetKey(req);
+    }
+
+    private ResultSetKey(SearchRequest req) {
+        this.agencyId = req.getAgency();
+        this.allObjects = req.getAllObjects();
+        this.collectionType = req.getCollectionTypeOrDefault();
+        this.profiles = req.getProfilesOrDefault();
+        this.query = req.getQuery();
+        this.repository = req.getRepository();
+        this.showAgencyId = req.getShowAgency();
+        this.sort = req.getSort();
+        this.userDefinedBoost = req.getUserDefinedBoost();
+        this.userDefinedRanking = req.getUserDefinedRanking();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.agencyId);
+        hash = 59 * hash + Objects.hashCode(this.allObjects);
+        hash = 59 * hash + Objects.hashCode(this.collectionType);
+        hash = 59 * hash + Objects.hashCode(this.profiles);
+        hash = 59 * hash + Objects.hashCode(this.query);
+        hash = 59 * hash + Objects.hashCode(this.repository);
+        hash = 59 * hash + Objects.hashCode(this.showAgencyId);
+        hash = 59 * hash + Objects.hashCode(this.sort);
+        hash = 59 * hash + Objects.hashCode(this.userDefinedBoost);
+        hash = 59 * hash + Objects.hashCode(this.userDefinedRanking);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        final ResultSetKey other = (ResultSetKey) obj;
+        return Objects.equals(this.query, other.query) &&
+               Objects.equals(this.repository, other.repository) &&
+               Objects.equals(this.agencyId, other.agencyId) &&
+               Objects.equals(this.allObjects, other.allObjects) &&
+               Objects.equals(this.collectionType, other.collectionType) &&
+               Objects.equals(this.profiles, other.profiles) &&
+               Objects.equals(this.showAgencyId, other.showAgencyId) &&
+               Objects.equals(this.sort, other.sort) &&
+               Objects.equals(this.userDefinedBoost, other.userDefinedBoost) &&
+               Objects.equals(this.userDefinedRanking, other.userDefinedRanking);
+    }
+
+}
